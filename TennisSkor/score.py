526ab030ffccbb6dd4e2a1530fd67d716a14a725
Achmad Irfan <achmad.irfan1@yahoo.com>
2026-03-14 10:48:57 +0700

    skor

--- TennisSkor/score.py
+++ TennisSkor/score.py
@@ -4,16 +4,52 @@ class Player:
         self.pt=0
         self.tb=0
         self.set=[0,0,0]
+        self.set_won=0
+        self.ace=0
+        self.df=0
+        self.winner=0
+        self.ue=0
+        self.servpersen=0
+        self.win1svr=0
+        self.win2svr=0
+        self.totalpoint=0
+        self.servepoint=0
+        self.returnpoint=0
+        
         
 class Match:
-    def __init__(self,p1,p2, max_game=6):
+    def __init__(self,p1,p2,firstserver):
         self.p1=Player(p1)
         self.p2=Player(p2)
-        self.max_game=max_game
         self.current_set=0
         self.tiebreak=False
+        self.current_tie_break=0
+        self.finish=False
+        self.winner=None
+        self.loser=None
+        self.score=[]
+        self.firstserver=firstserver
+        if firstserver == "p1":
+            self.current_server = self.p1
+        else:
+            self.current_server = self.p2
+        
+    def check_match_finish(self,player,opponent):
+        if player.set_won ==  2 or opponent.set_won == 2:
+            self.finish=True
+            if player.set_won > opponent.set_won:
+                self.winner= player
+                self.loser=opponent
+            else:
+                self.winner= opponent
+                self.loser= player
+                
+            self.scoring()
+            print("Finish")
         
     def tiebreak_scoring(self,player,opponent):
+        self.current_tie_break +=1
+        self.change_server_tiebreak()
         player.tb += 1
         if player.tb >= 7 and (player.tb - opponent.tb) >= 2:
             player.set[self.current_set] = 7
@@ -21,8 +57,10 @@ class Match:
             print("Set selesai via tiebreak")
             self.current_set += 1
             self.tiebreak = False
+            player.set_won +=1
             player.tb = 0
             opponent.tb = 0
+            self.check_match_finish(player,opponent)
             
     def check_tiebreak(self,player, opponent):
         print("Tie Break! Mode")
@@ -41,9 +79,17 @@ class Match:
             self.current_set += 1
             player.pt = 0
             opponent.pt = 0
+            player.set_won +=1
             
+    def win_game(self,player,opponent):
         
-
+        player.set[self.current_set] += 1
+        self.change_server()
+        player.pt = 0
+        opponent.pt = 0
+        self.check_set_finished(player, opponent)
+        self.check_match_finish(player,opponent)
+            
     def win_point(self,player,opponent):
         if player == "p1":
             player = self.p1
@@ -52,37 +98,73 @@ class Match:
             player = self.p2
             opponent = self.p1
             
+        if self.finish:
+            print(f"Pertandingan Selesai")
+            return
+            
         if self.tiebreak:
             print("MODE TIEBREAK START")
             self.tiebreak_scoring(player, opponent)
             return
         
+        player.totalpoint +=1
+        
+        
         if player.pt == 0:
             player.pt = 15
+           
         elif player.pt == 15:
             player.pt = 30
+           
         elif player.pt == 30:
             player.pt = 40
+           
         elif player.pt == 40 and opponent.pt == 40:
             player.pt = "AD"
             opponent.pt = "-"
+            
         elif player.pt == "-":
             player.pt = 40
             opponent.pt = 40
+           
         elif player.pt == "AD":
-            player.set[self.current_set] += 1
-            player.pt = 0
-            opponent.pt = 0
-            self.check_set_finished(player, opponent)
+            self.win_game(player, opponent)
+            
         elif player.pt == 40 and opponent.pt != 40:
-            player.set[self.current_set] += 1
-            player.pt = 0
-            opponent.pt = 0
-            self.check_set_finished(player, opponent)
-                
+            self.win_game(player, opponent)
+            
+            
+    def scoring(self):
+        for i in range(self.current_set):
+            if self.winner == self.p1:
+                self.score.append(f"{self.p1.set[i]}-{self.p2.set[i]}")
+            else:
+                self.score.append(f"{self.p2.set[i]}-{self.p1.set[i]}")
+
+        self.score = " ".join(self.score)
+        
+    def change_server(self):
+        if self.current_server == self.p1:
+            self.current_server = self.p2
+        else:
+            self.current_server = self.p1
+    
+    def change_server_tiebreak(self):
+        total = self.p1.tb + self.p2.tb
+
+        if total % 2 == 1:
+            self.change_server()
+        
+        
+        
     def get_score(self):
         score= {
-        "p1": {"pt": self.p1.pt, "set": self.p1.set, "tb":self.p1.tb},
-        "p2": {"pt": self.p2.pt, "set": self.p2.set, "tb":self.p2.tb} }
+        "p1": {"pt": self.p1.pt, "set": self.p1.set, "tb":self.p1.tb, "set_won": self.p1.set_won, "tp": self.p1.totalpoint},
+        "p2": {"pt": self.p2.pt, "set": self.p2.set, "tb":self.p2.tb, "set_won": self.p2.set_won, "tp": self.p2.totalpoint},
+        "finish": self.finish,
+        "result":{"winner":self.winner,"loser":self.loser},
+        "score": self.score,
+        "current_server": "p1" if self.current_server == self.p1 else "p2",
+        }
         
         return score
--- TennisSkor/views.py
+++ TennisSkor/views.py
@@ -2,19 +2,30 @@ from django.shortcuts import render
 from django.views.generic import View
 from . import score
 
-
-
 def Skor(request):
     
     # Ambil data user
-    p1 = request.GET.get("p1")
-    p2 = request.GET.get("p2")
+    p1 = request.GET.get("p1") or request.session.get("p1_name")
+    p2 = request.GET.get("p2") or request.session.get("p2_name")
+    print(f"p1 = {p1}")
+    print(f"p2 = {p2}")
+    
+    
+    # Pick first server
+    firstserver = request.GET.get("firstserve")
+    print(f"firstserver : {firstserver}")
+    print(f"Apakah firstserver None?  {firstserver==None}")
+    
+    
     
     # Cek session sebelumnya
     match = request.session.get('match')
     
     #Buat Objek
-    m = score.Match(p1,p2)
+    m = score.Match(p1, p2, firstserver or "p1")
+    
+    print(f"servise pertama = {m.current_server}")
+    
     if match and p1 == match.get("p1_name") and p2 == match.get("p2_name"):
         m.p1.pt = match["pt1"]
         m.p2.pt = match["pt2"]
@@ -22,9 +33,18 @@ def Skor(request):
         m.p2.set  = match["g2"]
         m.p1.tb = match["tb1"]
         m.p2.tb = match["tb2"]
+        m.p1.totalpoint = match["tp1"]
+        m.p2.totalpoint = match["tp2"]
         m.current_set = match["current_set"]
+        m.p1.set_won = match["set1_won"]
+        m.p2.set_won = match["set2_won"]
         m.tiebreak = match.get("tiebreak", False)
- 
+        m.finish= match['finish']
+        m.winner = match.get('winner')
+        m.loser = match.get('loser')
+        m.current_server = m.p1 if match['current_server'] == "p1" else m.p2
+            
+    
     # Cek Pemenang Poin
     pointWinner = request.POST.get("player")
     if pointWinner == "p1":
@@ -32,10 +52,8 @@ def Skor(request):
     elif pointWinner == "p2":
         m.win_point("p2","p1")
     
-    
     scores = m.get_score() 
     
-      
     request.session["match"] = {
         "p1_name": p1,
         "p2_name": p2,
@@ -45,8 +63,17 @@ def Skor(request):
         "g2": scores["p2"]['set'],
         "tb1": scores["p1"]['tb'],
         "tb2": scores["p2"]['tb'],
+        "tp1": scores["p1"]['tp'],
+        "tp2": scores["p2"]['tp'],
         "current_set": m.current_set,
+        "set1_won": scores["p1"]["set_won"],
+        "set2_won": scores["p2"]["set_won"],
         "tiebreak": m.tiebreak,
+        "winner": scores['result']['winner'].name if scores['result']['winner'] else None,
+        "loser": scores['result']['loser'].name if scores['result']['loser'] else None,
+        "finish": scores['finish'],
+        "score": scores['score'],
+        "current_server":scores['current_server']
     }
     
     context = {
@@ -62,10 +89,21 @@ def Skor(request):
         "set3_p2": scores["p2"]["set"][2],
         "tb1": scores["p1"]["tb"],
         "tb2": scores["p2"]["tb"],
+        "tp1": scores["p1"]["tp"],
+        "tp2": scores["p2"]["tp"],
         "current_set": m.current_set,
+        "set1_won": scores["p1"]["set_won"],
+        "set2_won": scores["p2"]["set_won"],
         "tiebreak": m.tiebreak,
+        "finish": scores['finish'],
+        "winner": scores['result']['winner'].name if scores['result']['winner'] else None,
+        "loser": scores['result']['loser'].name if scores['result']['loser'] else None,
+        "score": scores['score'],
+        "current_server":scores['current_server']
     }
     
+    
+    print(context)
     
     return render(request,'index.html', context)
             
