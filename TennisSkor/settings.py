c2ec98894ac94c15a97cb1e5b5df75c8c03713f9
Achmad Irfan <achmad.irfan1@yahoo.com>
2026-03-24 21:46:52 +0700

    setting env

--- TennisSkor/settings.py
+++ TennisSkor/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 import os
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-3#n*(6q6pb1@v^epv+_%88i19(db1p-cm7(f@_flpmmu4s*%x0'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
